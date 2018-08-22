a8c44d3b6f95dba78de029793e820a14c6ab566e
Juan Escobar <jdescobar1995@gmail.com>
2018-08-21 23:04:12 -0500

    Estructura lista y medias queries

--- application/view/_templates/header.php
+++ application/view/_templates/header.php
@@ -12,15 +12,28 @@
     <!-- CSS -->
     <link rel="stylesheet" type="text/css" href="<?php echo URL;?>css/resetStyles.css">
     <link rel="stylesheet" type="text/css" href="<?php echo URL;?>css/bootstrap.min.css">
-    <link rel="stylesheet" type="text/css" href="<?php echo URL;?>css/style.css">
+    <link rel="stylesheet" type="text/css" href="<?php echo URL;?>css/general.css">
+    <link rel="stylesheet" type="text/css" href="<?php echo URL;?>css/styles.css">
 </head>
 <body>
-<div class="container-fluid"><!--Se cierra en el footer-->
-  <div class="row">
-    <div class="col-md-2">
-      <p>Menu</p>
-    </div>
-    <div class="col-md-2">
-      <p>Logo</p>
+  <div class="container-fluid"><!--Se cierra en el footer-->
+    <div class="row">
+      <div class="col-2 col-md-2 col-sm-2 col-xs-2 centerElementsWidth centerElementsHeight">
+        <div class="dropdown">
+          <img src="<?php echo URL;?>/img/icons/hamburger.svg"  class= "nav-link dropdown-toggle inlineElement cursorPointer" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" alt=""> 
+          <p class="inlineElement">Menú</p>            
+          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item" href="#">Propuesta Pedagógica</a>
+            <a class="dropdown-item" href="#">Correo Corporativo</a>
+            <a class="dropdown-item" href="#">Sigma</a>
+            <a class="dropdown-item" href="#">Intranet</a>
+            <div class="dropdown-divider">
+            </div>
+            <a class="dropdown-item" href="#">Contáctenos</a>
+          </div>
+      </div>
+     </div>
+    <div class="col-10 col-md-10 col-sm-10 col-xs-10 centerElementsWidth">
+      <img src="<?php echo URL;?>img/LogoCoomulsap.jpg" class="img-responsive" alt="Logo Coomulsap" width="200">
     </div>
   </div>
--- application/view/home/index.php
+++ application/view/home/index.php
@@ -1,17 +1,16 @@
-<div class="row">
+<div class="row banner">
     <div class="col-md-12">
-        <p>Banner 30 años</p>
     </div>
 </div>
 <div class="row">
-    <div class="col-md-8">
+    <div class="col-md-8 color">
         <p>Banner Movil</p>
     </div>
-    <div class="col-md-4">
+    <div class="col-md-4 color">
         <p>Videos</p>
     </div>
 </div>
-<div class="row">
+<div class="row color">
     <div class="col-md-8">
         <p>Educación</p>
     </div>
@@ -19,7 +18,7 @@
         <p>EPJA</p>
     </div>
 </div>
-<div class="row">
+<div class="row color">
     <div class="col-md-4">
         <p>Invetigación</p>
     </div>
@@ -27,17 +26,17 @@
         <p>Producion</p>
     </div>
 </div>
-<div class="row">
-    <div class="col-md-3">
+<div class="row color">
+    <div class="col-md-3 color">
         <p>Fila 1</p>
     </div>
-    <div class="col-md-3">
+    <div class="col-md-3 color">
         <p>Fila 2</p>
     </div>
-    <div class="col-md-3">
+    <div class="col-md-3 color">
         <p>Fila 3</p>
     </div>
-    <div class="col-md-3">
+    <div class="col-md-3 color">
         <p>Fila 4</p>
     </div>
 </div>
