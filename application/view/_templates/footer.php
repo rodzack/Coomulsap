7bbdc62c00f7c9272d60df1f5eea419182a236b5
Juan Escobar <jdescobar1995@gmail.com>
2018-09-03 21:42:51 -0500

    Se agrego el FOoter

--- application/view/_templates/footer.php
+++ application/view/_templates/footer.php
@@ -1,7 +1,44 @@
 
-        <div class="row">
+        <div class="row footer">
             <div class="col-md-12">
-                <p>Información contacto</p>
+                <div class="row">
+                    <div class="col-md-4 color">
+                        <h4 class="greenTitle">Nuestra empresa</h4>
+                        <p class="paragraphGray">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin a ex in lacus elementum consequat. Nunc elementum a lectus eget facilisis. Morbi auctor mi sit amet elit ultricies, ut iaculis ante porttitor. Ut nulla libero, sodales quis volutpat vel, fermentum a ligula. Curabitur et lacinia neque, at convallis augue. Duis rhoncus dictum rhoncus. Suspendisse sit amet purus non neque sagittis facilisis. Quisque quis arcu at quam suscipit lobortis. Etiam mattis ipsum vitae velit tincidunt vehicula. Sed blandit vitae nulla eget tincidunt. Mauris turpis ex, porttitor a nisl eu, ullamcorper efficitur ligula. Vivamus suscipit quam pulvinar dui finibus mattis nec consectetur augue. Proin ultricies nec ligula quis dictum. Sed euismod a nunc non lacinia. Nulla nulla ipsum, euismod at aliquam eget, vehicula sed ipsum. In eleifend lacus eros, et elementum est efficitur sit amet.</p>
+                    </div>
+                    <div class="col-md-4 color">
+                        <b>Dirección:</b><p class="inlineElement">Calle 42 Sur # 69 A 58 San Antonio de Prado / Medellín</p> <br>
+                        <b>PBX:</b> <p class="inlineElement">(574) 444 42 62</p> <br/>
+                        <b>Email</b> <p class="inlineElement">coomulsap@coomulsap.com</p>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="row">
+                            <div class="col-md-2">
+                                <b>PQR</b>
+                            </div>
+                            <form>
+                                <div class="col-md-10">
+                                    <div class="form-group row">
+                                        <label for="input-name" class="col-3 col-form-label">Nombre</label>
+                                        <div class="col-9">
+                                            <input class="form-control" type="text" id="input-name">
+                                        </div>
+                                    </div>
+                                    <div class="form-group row">
+                                        <label for="input-email" class="col-3 col-form-label">Email</label>
+                                        <div class="col-9">
+                                            <input class="form-control" type="text" id="input-email">
+                                        </div>
+                                    </div>
+                                    <div class="form-group row">
+                                        <label for="comment">Comentarios:</label>
+                                        <textarea class="form-control" rows="5" id="comment"></textarea>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
--- application/view/home/index.php
+++ application/view/home/index.php
@@ -1,12 +1,40 @@
+<div class="row">
+    <div class="col-md-12 color">
+        <p>Banner Movil</p>
+        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+            <ol class="carousel-indicators">
+                <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+            </ol>
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                <img class="d-block w-100" src="<?php echo URL;?>/img/1.jpg" alt="First slide" height="300">
+                </div>
+                <div class="carousel-item">
+                <img class="d-block w-100" src="<?php echo URL;?>/img/2.jpg" alt="Second slide" height="300">
+                </div>
+                <div class="carousel-item">
+                <img class="d-block w-100" src="<?php echo URL;?>/img/3.jpg" alt="Third slide" height="300">
+                </div>
+            </div>
+            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Anterior</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Siguiente</span>
+            </a>
+            </div>
+    </div>
+</div>
 <div class="row banner">
     <div class="col-md-12">
     </div>
 </div>
-<div class="row">
-    <div class="col-md-8 color">
-        <p>Banner Movil</p>
-    </div>
-    <div class="col-md-4 color">
+<div class="row color">
+    <div class="col-md-12 color">
         <p>Videos</p>
     </div>
 </div>
